cd3c1dd2320beaa93c84f82b291fd954fe169e80
nimrod aviam <nimrod.a6@gmail.com>
2023-02-26 11:44:47 +0200

    click options, volume change

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 let intervalId;
 let bpm
+let volume = 1
+let click = "./sound/click.wav"
 
 console.log('popup.js go!')
 
@@ -16,7 +18,8 @@ function stop() {
 }
 
 function playClick() {
-    const audio = new Audio("./click.wav");
+    const audio = new Audio(click);
+    audio.volume = volume
     audio.play();
 }
 
@@ -30,11 +33,43 @@ function minus() {
     start()
 }
 
+function volumeChange(ev) {
+    const val = ev.target.value
+    console.log('val', val)
+    if (val === 'plus') {
+        console.log('up');
+        if (volume >= 1) return
+        else volume += 0.1
+        console.log('volume', volume)
+    }
+    if (val === 'minus') {
+        if (volume <= 0.3) return
+        else volume -= 0.1
+        console.log('volume', volume)
+    }
+}
+
+function soundChange(ev) {
+    const sound = ev.target.value;
+    console.log(sound);
+    if (sound === 'drum') click = './sound/click1.mp3'
+    if (sound === 'cowbell') click = './sound/click2.mp3'
+    if (sound === 'wood') click = './sound/click.wav'
+}
+
+
 document.getElementById('start').addEventListener('click', start);
 document.getElementById('stop').addEventListener('click', stop);
-document.getElementById('bpm').addEventListener('click', start);
+document.getElementById('bpm').addEventListener('change', start);
 document.getElementById('minus').addEventListener('click', minus);
 document.getElementById('plus').addEventListener('click', plus);
+document.getElementById('volumeUp').addEventListener('click', volumeChange);
+document.getElementById('volumeDown').addEventListener('click', volumeChange);
+document.getElementById('sound1').addEventListener('click', soundChange);
+document.getElementById('sound2').addEventListener('click', soundChange);
+document.getElementById('sound3').addEventListener('click', soundChange);
 
 
-dfg
+
+
+

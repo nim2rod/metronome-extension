527cccd189604587a97136cf3bec5209b2caa935
nimrod aviam <nimrod.a6@gmail.com>
2023-05-04 17:49:24 +0300

    version 2.0 - move from the prototype project

--- popup.js
+++ popup.js
@@ -1,83 +1,283 @@
 let intervalId;
-let bpm
 let volume = 1
-let click = "./sound/click.wav"
+let primaryClick = "./sound/digit.wav"
+let seconderyClick = "./sound/digit-up.wav"
 
-console.log('popup.js go!')
+let playIsOn = false;
+let GlobalSong = { songName: null, bpm: null }
+let songsFromStorage = []
+let division = 1
+let beatCount = 1
+let holdFirstBeat = true
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'Space') {
+        if (playIsOn) {
+            stop();
+            playIsOn = false;
+        } else {
+            start();
+            playIsOn = true;
+        }
+        event.preventDefault(); // prevent default space key behavior (scrolling)
+    }
+    if (event.code === 'Enter') addSong()
+    if (event.code === 'ArrowUp') volumeChange('up')
+    if (event.code === 'ArrowDown') volumeChange('down')
+});
+
+init()
+function init() {
+    const data = getFromLocal()
+    if (data && data.length) {
+        songsFromStorage = data
+        renderSongs(songsFromStorage)
+    }
+}
 
 function start() {
-    document.querySelector('.bpm-show').innerHTML = document.getElementById("bpm").value
-    console.log('start')
+    beatCount = 1
+    const bpmInit = document.getElementById("bpm").value
+    document.querySelector('.bpm-show').innerHTML = bpmInit
     clearInterval(intervalId);
-    bpm = parseInt(document.getElementById("bpm").value);
+    bpm = parseInt(bpmInit);
+    bpm = bpm * division
     intervalId = setInterval(playClick, (60 / bpm) * 1000);
-    console.log('bpm', bpm)
+    playIsOn = true
 }
 
 function stop() {
     clearInterval(intervalId);
+    playIsOn = false
+    beatCount = 1
 }
 
 function playClick() {
-    const audio = new Audio(click);
+    let audio = null
+    if (division === 1 || beatCount === 1) { //primary
+        audio = new Audio(primaryClick)
+    } else {                                   // secondery
+        audio = new Audio(seconderyClick);
+    }
     audio.volume = volume
     audio.play();
+    beatCount++
+    if (beatCount === division + 1) beatCount = 1
 }
 
+function moveBpmRange() {
+    const bpmChange = document.getElementById("bpm").value
+    if (GlobalSong.songName) {
+        GlobalSong.bpm = bpmChange
+        update()
+    }
+    start()
+}
 
 function bpmChange(ev) {
     const val = ev.target.value
     if (val === 'plus') {
         document.getElementById("bpm").value++
+        if (GlobalSong.songName) GlobalSong.bpm++
     }
     if (val === 'minus') {
         document.getElementById("bpm").value--
+        if (GlobalSong.songName) GlobalSong.bpm--
     }
-
+    if (GlobalSong.songName) {
+        update()
+    }
     start()
 }
 
 function volumeChange(ev) {
-    const val = ev.target.value
-    console.log('val', val)
+    let val = null
+    if (ev === 'up') val = 'up'
+    else if (ev === 'down') val = 'down'
+    else val = ev.target.dataset.value
     if (val === 'up') {
-        console.log('up');
         if (volume >= 1) return
         else volume += 0.1
-        console.log('volume', volume)
     }
     if (val === 'down') {
         if (volume <= 0.3) return
         else volume -= 0.1
-        console.log('volume', volume)
     }
 }
 
 function soundChange(ev) {
-    const sound = ev.target.value;
-    console.log(sound);
-    if (sound === 'drum') click = './sound/click1.mp3'
-    if (sound === 'cowbell') click = './sound/click2.mp3'
-    if (sound === 'wood') click = './sound/click.wav'
+    const sound = ev.target.dataset.value;
+    if (sound === 'stick') {
+        primaryClick = './sound/stick.wav'
+        seconderyClick = './sound/stick-up.wav'
+    }
+    if (sound === 'cowbell') {
+        primaryClick = './sound/bell.wav'
+        seconderyClick = './sound/bell-up.wav'
+    }
+    if (sound === 'met') {
+        primaryClick = './sound/met.wav'
+        seconderyClick = './sound/met-up.wav'
+    }
+    if (sound === 'pulse') {
+        primaryClick = './sound/digit.wav'
+        seconderyClick = './sound/digit-up.wav'
+    }
+    beatCount = 1
 }
 
-function log() {
-    console.log('asdfafasfsf')
+function addSong() {
+    let bpm = document.getElementById("bpm").value
+    let trackName = document.getElementById("input-song").value
+    if (!trackName) return
+    const checkIfUniq = songsFromStorage.find((song) => song.songName === trackName)
+    if (checkIfUniq) {
+        document.querySelector('.input-song').value = ``
+        return
+    }
+
+    const newSong = { songName: trackName, bpm: bpm }
+    GlobalSong = { ...newSong }
+    copyGlobal = { ...newSong }
+    songsFromStorage.push(copyGlobal)
+    setToLocal(songsFromStorage)
+    renderSongs(songsFromStorage)
+
+    document.querySelector('.input-song').value = ``
+}
+
+function pickNewSong(bpm, trackName) {
+    GlobalSong = { songName: trackName, bpm: bpm }
+    document.getElementById("bpm").value = bpm
+    document.querySelector('.bpm-show').innerHTML = bpm
+    stop()
+    // start()
+}
+
+function renderSongs(songs) {
+    document.querySelector('.list').innerHTML = ``
+    songs.forEach((song) => {
+        document.querySelector('.list').innerHTML += `
+        <div id="song-line" class="song-line btn">
+        <span id="song-name" class="song-name">${song.songName}</span>
+        <span class="song-line-right"><span id="song-bpm" class="song-bpm">${song.bpm}</span> 
+        <img id="delete-song" class="delete-song btn" src="./icons/x-mark.png" alt="" data-value="stick">
+        </span></div>
+        `
+    })
+
+    const songLines = document.querySelectorAll('#song-line');
+    songLines.forEach(songLine => {
+        const lineBpm = songLine.querySelector('.song-bpm').textContent;
+        const lineTrackName = songLine.querySelector('.song-name').textContent;
+        songLine.addEventListener('click', function () {
+            pickNewSong(lineBpm, lineTrackName);
+            songLines.forEach(line => {
+                line.classList.remove('selected')
+            });
+
+            this.classList.add('selected')
+        });
+        if (lineTrackName === GlobalSong.songName) songLine.classList.add('selected')
+
+        const deleteBtn = songLine.querySelector('.delete-song')
+        const songToDelete = { songName: lineTrackName, bpm: lineBpm }
+        deleteBtn.addEventListener('click', function () {
+            remove(songToDelete)
+        })
+    });
+}
+
+function getFromLocal() {
+    // let res
+    // chrome.storage.local.get("songsList", function (result) {
+    //     res = JSON.parse(result.songsList)
+    // })
+    // return res
+    let data = localStorage.getItem('songsList')
+    return JSON.parse(data)
+}
+
+function setToLocal(songs) {
+    localStorage.setItem('songsList', JSON.stringify(songs))
+}
+
+function update() {
+    let songToReplace = { ...GlobalSong }
+    const index = songsFromStorage.findIndex((song) => {
+        return song.songName === songToReplace.songName
+    })
+    songsFromStorage.splice(index, 1, songToReplace)
+
+    setToLocal(songsFromStorage)
+    renderSongs(songsFromStorage)
+}
+
+function remove(songToDelete) {
+    const index = songsFromStorage.findIndex((song) => {
+        return song.songName === songToDelete.songName
+    })
+    songsFromStorage.splice(index, 1)
+    setToLocal(songsFromStorage)
+    renderSongs(songsFromStorage)
 }
 
 document.getElementById('start').addEventListener('click', start);
 document.getElementById('stop').addEventListener('click', stop);
-// document.getElementById('bpm').addEventListener('change', start);
-document.getElementById('bpm').addEventListener('input', start);
+document.getElementById('bpm').addEventListener('input', moveBpmRange);
 document.getElementById('minus').addEventListener('click', bpmChange);
 document.getElementById('plus').addEventListener('click', bpmChange);
 document.getElementById('volumeUp').addEventListener('click', volumeChange);
 document.getElementById('volumeDown').addEventListener('click', volumeChange);
-document.getElementById('sound1').addEventListener('click', soundChange);
-document.getElementById('sound2').addEventListener('click', soundChange);
-document.getElementById('sound3').addEventListener('click', soundChange);
-
+document.getElementById('add-song').addEventListener('click', addSong);
+const sounds = document.querySelectorAll('.sound');
+sounds.forEach(sound => {
+    sound.addEventListener('click', soundChange);
+});
 document.querySelector('.bpm-show').innerHTML = document.getElementById("bpm").value
+
+const tapTempoButton = document.getElementById('tap-tempo');
+let tapTempoTimes = [];
+let lastTimeClicked = 0;
+
+tapTempoButton.addEventListener('click', function () {
+    let tempo
+    const now = performance.now();
+    if (lastTimeClicked !== 0) {
+        const timeDiff = now - lastTimeClicked;
+        if (lastTimeClicked !== 0 && timeDiff > 2500) tapTempoTimes = []
+        else tapTempoTimes.push(timeDiff);
+        if (tapTempoTimes.length > 4) {
+            tapTempoTimes.shift();
+        }
+        const averageTime = tapTempoTimes.reduce((a, b) => a + b, 0) / tapTempoTimes.length;
+        tempo = Math.round(60000 / averageTime);
+    }
+    lastTimeClicked = now;
+    if (tapTempoTimes.length > 1) {
+        document.getElementById("bpm").value = tempo
+        if (GlobalSong.songName) {
+            GlobalSong.bpm = tempo
+            update()
+        }
+        start()
+    }
+
+});
+
+const bpmDivides = document.querySelectorAll('.divide-box span');
+bpmDivides.forEach((divide) => {
+    divide.addEventListener('click', (ev) => {
+        division = +ev.target.dataset.value
+        start()
+        bpmDivides.forEach((divide) => divide.classList.remove('active'));
+        divide.classList.add('active');
+    });
+    bpmDivides[0].classList.add('active');
+});
+
+
+
 
 
 
